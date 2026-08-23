28245f35b0641c2011726c43744a071ad1343eb1
Flow <cipianon@gmail.com>
2026-08-23 06:04:35 +0000

    fix chart redraw

--- atomic_defi_design/imports/coinpaprika/dist/widget.js
+++ atomic_defi_design/imports/coinpaprika/dist/widget.js
@@ -11780,25 +11780,12 @@ class chartClass {
           if (this.chart) {
             const axis = this.chart.xAxis[0];
             const priceSeries = this.getPriceSeries();
-            let dataMin, dataMax;
             if (priceSeries && priceSeries.xData && priceSeries.xData.length > 0) {
-              dataMin = priceSeries.xData[0];
-              dataMax = priceSeries.xData[priceSeries.xData.length - 1];
-              console.log("Series length: " + priceSeries.xData.length);
-              console.log("First point timestamp: " + dataMin);
-              console.log("Last point timestamp: " + dataMax);
-            }
-            console.log("Before reflow - axis.min: " + axis.min + " axis.max: " + axis.max);
-            console.log("Before reflow - axis.dataMin: " + axis.dataMin + " axis.dataMax: " + axis.dataMax);
-            this.chart.reflow();
-            console.log("After reflow - axis.min: " + axis.min + " axis.max: " + axis.max);
-            if (dataMin !== undefined && dataMax !== undefined) {
-              console.log("Calling setExtremes with dataMin: " + dataMin + " dataMax: " + dataMax);
+              const dataMin = priceSeries.xData[0];
+              const dataMax = priceSeries.xData[priceSeries.xData.length - 1];
               axis.setExtremes(dataMin, dataMax, false, false);
+              this.chart.redraw(false);
             }
-            console.log("Before redraw - axis.min: " + axis.min + " axis.max: " + axis.max);
-            this.chart.redraw(false);
-            console.log("After redraw - axis.min: " + axis.min + " axis.max: " + axis.max);
           }
           resolve();
         }, 100);
