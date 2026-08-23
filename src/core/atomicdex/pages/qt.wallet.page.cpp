dd05afc5b56945e25af6f5fc9c0a7ca2bcf85375
Flow <cipianon@gmail.com>
2026-08-23 13:34:00 +0000

    remove check_send_availability loop in wallet

--- src/core/atomicdex/pages/qt.wallet.page.cpp
+++ src/core/atomicdex/pages/qt.wallet.page.cpp
@@ -37,19 +37,18 @@ namespace atomic_dex
     void
     wallet_page::update()
     {
-        if (!m_page_open)
-        {
-            return;
-        }
-        using namespace std::chrono_literals;
-
-        const auto now = std::chrono::high_resolution_clock::now();
-        const auto s   = std::chrono::duration_cast<std::chrono::seconds>(now - m_update_clock);
-        if (s >= 5s)
-        {
-            check_send_availability();
-            m_update_clock = std::chrono::high_resolution_clock::now();
-        }
+//        if (!m_page_open)
+//        {
+//            return;
+//        }
+//        using namespace std::chrono_literals;
+//        const auto now = std::chrono::high_resolution_clock::now();
+//        const auto s   = std::chrono::duration_cast<std::chrono::seconds>(now - m_update_clock);
+//        if (s >= 5s)
+//        {
+//            check_send_availability();
+//            m_update_clock = std::chrono::high_resolution_clock::now();
+//        }
     }
 } // namespace atomic_dex
 
@@ -59,8 +58,6 @@ namespace atomic_dex
     void
     wallet_page::check_send_availability()
     {
-        spdlog::stopwatch sw; using namespace std::chrono;
-
         auto& kdf              = m_system_manager.get_system<kdf_service>();
         auto  global_coins_cfg = m_system_manager.get_system<portfolio_page>().get_global_cfg();
         auto  ticker_info      = global_coins_cfg->get_coin_info(kdf.get_current_ticker());
@@ -95,11 +92,10 @@ namespace atomic_dex
                 m_current_ticker_fees_coin_enabled = true;
             }
         }
+
         emit sendAvailableChanged();
         emit sendAvailabilityStateChanged();
         emit currentTickerFeesCoinEnabledChanged();
-
-        SPDLOG_DEBUG("Time elapsed in wallet_page::check_send_availability for ticker {} with result {}: {}", ticker_info.ticker, m_send_available, duration_cast<milliseconds>(sw.elapsed()));
     }
 } // namespace atomic_dex
 
@@ -422,7 +418,6 @@ namespace atomic_dex
         m_claiming_rpc_result = rpc_data.toJsonObject();
         emit claimingRpcDataChanged();
     }
-
 
     QVariant
     wallet_page::get_rpc_claiming_faucet_data() const
--- src/core/atomicdex/pages/qt.wallet.page.hpp
+++ src/core/atomicdex/pages/qt.wallet.page.hpp
@@ -167,7 +167,7 @@ namespace atomic_dex
         bool                                           m_send_available{true};
         QString                                        m_send_availability_state;
         bool                                           m_current_ticker_fees_coin_enabled{true}; // Tells if the current ticker's fees coin is enabled.
-        std::chrono::high_resolution_clock::time_point m_update_clock;                           // Clock used to time the `update()` loop of this ecs system.
+        //std::chrono::high_resolution_clock::time_point m_update_clock;                           // Clock used to time the `update()` loop of this ecs system.
         bool                                           m_page_open{false};
     };
 } // namespace atomic_dex
