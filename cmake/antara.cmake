e9a6fb8c4bbc1fc35e5c1aaf94791b68e246158a
Flow <cipianon@gmail.com>
2026-09-03 14:03:41 +0000

    revert back to linuxdeployqt

--- cmake/antara.cmake
+++ cmake/antara.cmake
@@ -69,28 +69,6 @@ macro(target_enable_coverage target)
     endif ()
 endmacro()
 
-macro(download_app_image)
-    if (LINUX)
-        ## We need appimage
-        if (NOT EXISTS ${PROJECT_SOURCE_DIR}/tools/linux/linuxdeploy-x86_64.AppImage)
-            file(DOWNLOAD
-                    https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-                    ${PROJECT_SOURCE_DIR}/tools/linuxdeploy-x86_64.AppImage
-                    SHOW_PROGRESS
-                    )
-        endif ()
-        if (EXISTS ${PROJECT_SOURCE_DIR}/tools/linuxdeploy-x86_64.AppImage)
-            file(COPY
-                    ${PROJECT_SOURCE_DIR}/tools/linuxdeploy-x86_64.AppImage DESTINATION
-                    ${PROJECT_SOURCE_DIR}/tools/linux/
-                    FILE_PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ)
-        endif ()
-        if (EXISTS ${PROJECT_SOURCE_DIR}/tools/linuxdeploy-x86_64.AppImage)
-            file(REMOVE ${PROJECT_SOURCE_DIR}/tools/linuxdeploy-x86_64.AppImage)
-        endif ()
-    endif ()
-endmacro()
-
 macro(target_enable_asan target)
     message("-- ASAN Enabled, Configuring...")
     target_compile_options(${target} PUBLIC
@@ -98,7 +76,6 @@ macro(target_enable_asan target)
     target_link_options(${target} PUBLIC
             $<$<AND:$<CONFIG:Debug>,$<NOT:$<CXX_COMPILER_ID:MSVC>>>:-fsanitize=address -fno-omit-frame-pointer>)
 endmacro()
-
 
 macro(target_enable_tsan target)
     if (USE_TSAN AND NOT ASAN AND NOT EMSCRIPTEN)
@@ -129,21 +106,9 @@ macro(magic_game_app_image_generation from_dir desktop_file appdata_file app_ico
                 RUNTIME_OUTPUT_DIRECTORY_DEBUG ${output_dir}/usr/bin
                 RUNTIME_OUTPUT_DIRECTORY_RELEASE ${output_dir}/usr/bin)
         file(COPY ${assets_dir} DESTINATION ${output_dir}/usr/share/)
-        if (BUILD_WITH_APPIMAGE)
-            configure_file(${from_dir}/${desktop_file} ${output_dir}/usr/share/applications/${desktop_file} COPYONLY)
-            configure_file(${from_dir}/${appdata_file} ${output_dir}/usr/share/metainfo/${appdata_file} COPYONLY)
-            configure_file(${from_dir}/${app_icon} ${output_dir}/usr/share/icons/hicolor/128x128/apps/${app_icon} COPYONLY)
-            add_custom_command(TARGET ${target}
-                    POST_BUILD COMMAND
-                    bash -c
-                    "ARCH=x86_64 VERSION=${PROJECT_VERSION} ${PROJECT_SOURCE_DIR}/tools/linux/linuxdeploy-x86_64.AppImage --appdir ${output_dir} --output appimage"
-                    $<TARGET_FILE:${target}>
-                    WORKING_DIRECTORY ${exe_runtime_directory})
-        endif ()
     endif ()
 endmacro()
 
 macro(init_antara_env)
     init_apple_env()
-    download_app_image()
 endmacro()
