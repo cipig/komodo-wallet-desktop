a69d87a13aebc4c140ad728c4433ef7118bf2c47
Flow <cipianon@gmail.com>
2026-09-17 02:50:47 +0000

    performance

--- src/core/atomicdex/models/qt.orders.model.cpp
+++ src/core/atomicdex/models/qt.orders.model.cpp
@@ -764,7 +764,7 @@ namespace atomic_dex
             this->set_recover_fund_busy(false);
         };
 
-        kdf_system.get_kdf_client().async_rpc_batch_standalone(batch).then(
+        kdf_system.get_kdf_client().async_rpc_batch_standalone(std::move(batch)).then(
             [this, answer_functor](async::task<t_http_response> previous_task)
             {
                 try
